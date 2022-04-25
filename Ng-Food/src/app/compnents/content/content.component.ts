ef5f2cd3bf83b61f282e857de8ede57e1f09a388
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-04-25 00:48:30 -0300

    Exercicio 06 semana 08 curso DevInHouse[NDD]

--- Ng-Food/src/app/compnents/content/content.component.ts
+++ Ng-Food/src/app/compnents/content/content.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+interface cards {
+  titulo: string;
+  imagem: string;
+}
 @Component({
   selector: 'NGF-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
+  listaCards: cards[] = [
+    {
+      titulo: 'Comida',
+      imagem: 'assets/comida.jpg',
+    },
+    {
+      titulo: 'Bebida',
+      imagem: 'assets/bebida.jpg',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
