5c114688c01edf3b30c310263c8c56dfdcc73245
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-04-24 21:00:23 -0300

    Exercicio 03 semana 08 curso DevInHouse[NDD]

--- Ng-Food/src/app/app.module.ts
+++ Ng-Food/src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
