22a272ed94546d1acbb1d6eaa69cfee2b6a72644
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2022-05-11 17:54:53 -0300

    Exercicio 01 semana 10 curso DevInHouse[NDD]

--- Ng-Food/src/app/app.module.ts
+++ Ng-Food/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { BebidaComponent } from './pages/bebida/bebida.component';
 import { ComidaListaComponent } from './compnents/comida-lista/comida-lista.component';
 import { BebidaListaComponent } from './compnents/bebida-lista/bebida-lista.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ItemCardapioComponent } from './compnents/item-cardapio/item-cardapio.component';
 
 const ROUTES: Route[] = [
   {
@@ -38,6 +39,7 @@ const ROUTES: Route[] = [
     BebidaComponent,
     ComidaListaComponent,
     BebidaListaComponent,
+    ItemCardapioComponent,
   ],
   imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES)],
 
